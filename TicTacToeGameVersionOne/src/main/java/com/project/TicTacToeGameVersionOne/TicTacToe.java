a40f9f15e4ef67d3ca7c6ec26dfb881da9470ceb
deepa2695 <deepamano2695@gmail.com>
2021-09-18 06:58:42 -0700

    refactor: Refactored for better readability

--- TicTacToeGameVersionOne/src/main/java/com/project/TicTacToeGameVersionOne/TicTacToe.java
+++ TicTacToeGameVersionOne/src/main/java/com/project/TicTacToeGameVersionOne/TicTacToe.java
@@ -7,6 +7,10 @@ public class TicTacToe {
 	}
 
 	static String playTicTacToe() {
+		return welcomeMessage();
+	}
+
+	protected static String welcomeMessage() {
 		String welcomeMsg = "Welcome To Tic Tac Toe Game..!!!";
 		System.out.println(welcomeMsg);
 		return welcomeMsg;
