0f57b45bc44ba33531fb8f2c97f1ab7f33d1302b
deepa2695 <deepamano2695@gmail.com>
2021-09-18 08:27:42 -0700

    feat: Added the feature by adding numbers in game board boxes

--- TicTacToeGameVersionOne/src/main/java/com/project/TicTacToeGameVersionOne/TicTacToe.java
+++ TicTacToeGameVersionOne/src/main/java/com/project/TicTacToeGameVersionOne/TicTacToe.java
@@ -8,6 +8,24 @@ public class TicTacToe {
 		playTicTacToe();
 
 		createGameBoardNumbers();
+		
+		createGameBoard();
+	}
+
+	protected static void createGameBoard() {
+		System.out.println("|---|---|---|");
+		System.out.println("| " + board[0] + " | "
+						+ board[1] + " | " + board[2]
+						+ " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[3] + " | "
+						+ board[4] + " | " + board[5]
+						+ " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[6] + " | "
+						+ board[7] + " | " + board[8]
+						+ " |");
+		System.out.println("|---|---|---|");
 	}
 
 	protected static String[] createGameBoardNumbers() {
