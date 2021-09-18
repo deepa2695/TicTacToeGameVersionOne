8cfbeffaa0ff6297718ebf8376e0abcdb28ef263
deepa2695 <deepamano2695@gmail.com>
2021-09-18 08:36:36 -0700

    test: Refactored the test class

--- TicTacToeGameVersionOne/src/main/java/com/project/TicTacToeGameVersionOne/TestTicTacToe.java
+++ TicTacToeGameVersionOne/src/main/java/com/project/TicTacToeGameVersionOne/TestTicTacToe.java
@@ -5,25 +5,25 @@ import org.junit.Test;
 import junit.framework.Assert;
 
 public class TestTicTacToe {
-	
+
 	@Test
-	public void returnsWelcomeMessage(){
+	public void returnsWelcomeMessage() {
 		Assert.assertEquals("Welcome To Tic Tac Toe Game..!!!", TicTacToe.playTicTacToe());
 	}
-	
+
 	@Test
-	public void shouldReturnTrueIfBoardNumbersAreEqualToGivenInput(){
-		String[] output = TicTacToe.createGameBoardNumbers() ;
-		String[] expected = new String[] {"1", "2", "3","4", "5", "6","7", "8", "9"};
-		for(int a = 0; a < 9; a++){
-			Assert.assertEquals(expected[a], output[a]); 
+	public void shouldReturnTrueIfBoardNumbersAreEqualToGivenInput() {
+		String[] output = TicTacToe.createGameBoardNumbers();
+		String[] expected = new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+		for (int a = 0; a < 9; a++) {
+			Assert.assertEquals(expected[a], output[a]);
 		}
 	}
-	
+
 	@Test
-	public void shouldReturnTrueIfGivenInputMessageIsEqualToOutput(){
-		Assert.assertEquals("X will play first. Enter a slot number to place X in:", TicTacToe.messageToEnterNumberToXPlayer());
+	public void shouldReturnTrueIfGivenInputMessageIsEqualToOutput() {
+		Assert.assertEquals("X will play first. Enter a slot number to place X in:",
+				TicTacToe.messageToEnterNumberToXPlayer());
 	}
-	
-	
+
 }
